b8f3f5a3f331f52d2e397298753d810bd452792e
Tibor Foris <tibor.foris@dpc.hu>
2018-12-17 12:32:29 +0100

    Removed old scripts directory

--- src/main/scripts/TimeCheck Client.js
+++
@@ -1,7 +0,0 @@
-var epoch = new Date().getTime() / 1000;
-console.log("Current epoch is " + epoch);
-// Use the following two lines for custom authentication node to receive the value
-document.getElementById("output").value = epoch;
-document.getElementById("loginButton_0").click();
-// Use the following line for a standard scripted authentication module return value
-// output.value = epoch;
